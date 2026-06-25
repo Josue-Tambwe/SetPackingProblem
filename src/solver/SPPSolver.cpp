1a8568ad11f7e4d603dfe17da829d0933f897d4a
Josue-Tambwe <tambwejosue1@gmail.com>
2026-06-25 23:36:18 +0200

    Update

--- src/solver/SPPSolver.cpp
+++ src/solver/SPPSolver.cpp
@@ -27,6 +27,12 @@
    #include "dataStructures/Timer.hpp" // to remove
    #include "algorithms/localSearch/VariableNeighborhoodDescent.hpp" // to remove
    #include "algorithms/greedy/RunGreedy.hpp"
+   #include "dataStructures/BaBNode.hpp"
+   #include "algorithms/branchAndBound/BestFirst.hpp" // to remove
+   #include "algorithms/branchAndBound/DepthFirst.hpp" // to remove
+   #include "algorithms/milpSolver/GurobiBackend.hpp" // to remove
+   #include "algorithms/milpSolver/HighsBackend.hpp" // to remove
+   #include "algorithms/milpSolver/HexalyBackend.hpp" // to remove
 
    
    using namespace spp; 
@@ -35,7 +41,58 @@
 
       const Params params = parseOptions(argc, argv);
 
-      if(params.algorithm == Algorithm::Greedy){runGreedy(params);}
+      //if(params.algorithm == Algorithm::Greedy){runGreedy(params);}
+
+      const Instance instance(params);
+      Timer timer;
+      GurobiBackend lp_gurobi;
+      HighsBackend lp_highs;
+
+      timer.start();
+      lp_gurobi.buildContinuousModel(instance);
+      lp_gurobi.solveContinuousModel();
+      timer.stop();
+
+      std::cout << " LP sol gurobi : " << lp_gurobi.getObjectiveValue() << " time : " << timer.getElapsedTime() << " (s) \n\n";
+
+      timer.reset();
+      timer.start();
+      lp_highs.buildContinuousModel(instance);
+      lp_highs.solveContinuousModel();
+      timer.stop();
+
+      std::cout << " LP sol highs  : " << lp_highs.getObjectiveValue() << " time : " << timer.getElapsedTime() << " (s) \n\n";
+
+      GurobiBackend ip_gurobi;
+      timer.reset();
+      timer.start();
+      ip_gurobi.buildIntegerModel(instance);
+      ip_gurobi.solveIntegerModel(120.0);
+      timer.stop();
+
+      std::cout << " IP sol gurobi : " << ip_gurobi.getObjectiveValue() << " time : " << timer.getElapsedTime() << " (s) \n\n";
+
+
+      HighsBackend ip_highs;
+      timer.reset();
+      timer.start();
+      ip_highs.buildIntegerModel(instance);
+      ip_highs.solveIntegerModel(120.0);
+      timer.stop();
+
+      std::cout << " IP sol highs  : " << ip_highs.getObjectiveValue() << " time : " << timer.getElapsedTime() << " (s) \n\n";
+
+
+      HexalyBackend hexaly;
+      timer.reset();
+      timer.start();
+      hexaly.buildIntegerModel(instance);
+      hexaly.solveIntegerModel(120.0);
+      timer.stop();
+
+      std::cout << " IP sol hexaly  : " << hexaly.getObjectiveValue() << " time : " << timer.getElapsedTime() << " (s) \n\n";
+
+
       
       
 
