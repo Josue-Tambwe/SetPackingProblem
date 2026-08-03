6be547e3bda1e9a6ff421091751da1f091b48986
Josue-Tambwe <tambwejosue1@gmail.com>
2026-08-03 23:18:38 +0200

    Update

--- src/output/GeneralOutput.cpp
+++ src/output/GeneralOutput.cpp
@@ -50,13 +50,16 @@
                   << "Path to the instance file\n\n"
 
                   << "  " << "--update-interval=value" << std::string(5, ' ')
-                  << "Number of iterations before updating alpha probabilities for the GRASP Algorithm \n\n"
+                  << "Number of iterations before updating alpha probabilities for the GRASP algorithm \n\n"
 
                   << "  " << "--tabu-tenure=value" << std::string(9, ' ')
                   << "Number of iterations that a move remains tabu for the Tabu Search Algorithm \n\n"
 
                   << "  " << "--restart-interval=value" << std::string(4, ' ')
-                  << "Number of iterations before rebulding the current solution for the Tabu Search Algorithm \n\n"
+                  << "Number of iterations before rebuilding the current solution for the Tabu Search and Simulated Annealing\n\n"
+
+                  << "  " << "--cooling-interval=value" << std::string(4, ' ')
+                  << "Number of iterations between two temperature updates in the Simulated Annealing schedule\n\n"
 
                   << "  " << "--pop-size=value" << std::string(12, ' ')
                   << "Population size for the Genetic Algorithm \n\n"
@@ -68,56 +71,65 @@
 
     void printOptionalOptions(){
 
-        std::cout << " Optional options:\n" << " " << std::string(18, '-') << "\n\n"
+        std::cout << " Optional options:\n" << " " << std::string(23, '-') << "\n\n"
 
-                << "  " << "--simd" << std::string(22, ' ')
+                << "  " << "--simd" << std::string(27, ' ')
                 << "Enable SIMD-accelerated kernels (AVX2/NEON)\n \n"
 
-                << "  " << "--warm-start" << std::string(16, ' ')
+                << "  " << "--warm-start" << std::string(21, ' ')
                 << "Provide a feasible starting point for the MILP resolution approach\n \n"
 
-                << "  " << "--verbose" << std::string(19, ' ')
+                << "  " << "--verbose" << std::string(24, ' ')
                 << "Enable verbose mode\n \n"
 
-                << "  " << "--intensification" << std::string(11, ' ')
+                << "  " << "--intensification" << std::string(16, ' ')
                 << "Enable an intensified Variable Neighborhood Descent local search\n \n"
 
-                << "  " << "--path-relinking" << std::string(12, ' ')
+                << "  " << "--path-relinking" << std::string(17, ' ')
                 << "Enable the Path-Relinking procedure to intensify the GRASP algorithm\n \n"
 
-                << "  "  << "--nb-threads=value" << std::string(10, ' ')
+                << "  "  << "--nb-threads=value" << std::string(15, ' ')
                 << "Number of threads to use (default: number of physical CPU cores)\n \n"
 
-                << "  "  << "--time-limit=value" << std::string(10, ' ')
+                << "  "  << "--time-limit=value" << std::string(15, ' ')
                 << "Set the time limit in seconds (default: 10)\n \n"
 
-                << "  "  << "--branching-rule=value" << std::string(6, ' ')
+                << "  "  << "--branching-rule=value" << std::string(11, ' ')
                 << "Branching rule strategy for the Branch and Bound algorithm (values: one, zero, fractional)\n \n"
 
-                << "  "  << "--exploration=value" << std::string(9, ' ')
+                << "  "  << "--exploration=value" << std::string(14, ' ')
                 << "Node exploration strategy for the Branch and Bound algorithm (values: bfs, dfs)\n \n"
 
-                << "  "  << "--gap=value" << std::string(17, ' ')
+                << "  "  << "--gap=value" << std::string(22, ' ')
                 << "Target optimality gap in [0,1] for the Branch and Bound algorithm (default: 0.0)\n \n"
 
-                << "  "  << "--iterations=value" << std::string(10, ' ')
+                << "  "  << "--iterations=value" << std::string(15, ' ')
                 << "Number of iterations for Genetic Algorithm, Simulated Annealing and Tabu Search (default: 1)\n \n"
 
-                << "  "  << "--nb-cycles=value" << std::string(11, ' ')
+                << "  "  << "--nb-cycles=value" << std::string(16, ' ')
                 << "Number of cycles for GRASP. Each cycle runs update-interval iterations before updating alpha probabilities\n \n"
 
-                << "  "  << "--nb-elites=value" << std::string(11, ' ')
+                << "  "  << "--nb-elites=value" << std::string(16, ' ')
                 << "Number of elite solutions within the elite solutions pool for Reactive GRASP with Path-Relinking\n \n"
                 
                 
-                << "  "  << "--bias=value" << std::string(16, ' ')
+                << "  "  << "--bias=value" << std::string(21, ' ')
                 << "Bias factor in [0,1] for GRASP and Genetic algorithm (low = 0.0, high = 1.0, default: 0.0)\n \n"
 
-                << "  "  << "--alpha=value" << std::string(15, ' ')
+                << "  "  << "--alpha=value" << std::string(20, ' ')
                 << "Greedy-randomness level in [0,1] (low = highly random, high = highly greedy, default: 0.5)\n\n"
 
-                << "  "  << "--pruning-rate=value" << std::string(8, ' ')
-                << "Proportion in [0,1] of elite inactive variables tested in restricted 1-2 and 2-1 neighborhoods\n\n\n \n";
+                << "  "  << "--pruning-rate=value" << std::string(13, ' ')
+                << "Proportion in [0,1] of elite inactive variables tested in restricted 1-2 and 2-1 neighborhoods\n\n"
+
+                << "  "  << "--cooling-factor=value" << std::string(11, ' ')
+                << "Geometric decay coefficient in [0,1] applied to the temperature at each cooling interval\n\n"
+
+                << "  "  << "--initial-temperature=value" << std::string(6, ' ')
+                << "Starting temperature of the Simulated Annealing schedule (higher values accept more worsening moves)\n\n"
+
+                << "  "  << "--final-temperature=value" << std::string(8, ' ')
+                << "Minimum temperature allowed by the cooling schedule. The algorithm stops when it is reached\n\n\n";
 
     }
 
@@ -138,6 +150,9 @@
 
             << "    ./bin/spp_solver --algorithm=ts "
             << "--instance=benchmarks/pb_1000rnd0700.dat  --tabu-tenure=10 --restart-interval=50  --time-limit=60  --simd\n\n"
+
+            << "    ./bin/spp_solver --algorithm=sa "
+            << "--instance=benchmarks/pb_1000rnd0700.dat --cooling-interval=100 --alpha=0.75 --cooling-factor=0.95  --simd\n\n"
 
             << "    ./bin/spp_solver --algorithm=bab "
             << "--instance=benchmarks/pb_1000rnd0700.dat  --intensification  --solver=gurobi --exploration=dfs\n\n"
